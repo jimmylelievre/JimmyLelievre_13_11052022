0ec6661c6473b59ab5a641f9d79dbf0bf052450d
jimmy lelievre <jimmylelievre@iMac-de-jimmy.local>
2022-05-19 17:43:50 +0200

    responsive design account

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,12 @@
 import { useSelector } from "react-redux";
 
+/**
+ * Check if user logged
+ * @returns
+ */
+
 function useLogged() {
+  // Get state 'authentification' from auth.js
   const logged = useSelector((state) => state.auth.isAuthentified);
   console.log(logged);
   return logged;
